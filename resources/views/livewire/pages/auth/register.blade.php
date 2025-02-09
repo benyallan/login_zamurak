ead70deaa445239a89390e65ab5bb6e141d3ae78
Beny Allan <benyallan@yahoo.com.br>
2025-02-09 12:03:56 -0300

    Atualiza o formulário de registro para incluir campo de nome de usuário, melhora o estilo e a disposição dos elementos.

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -40,52 +40,56 @@ $register = function () {
 ?>
 
 <div>
-    <form wire:submit="register">
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+    <img src="{{ asset('storage/images/fundo_register.png') }}" alt="" class="absolute top-0 left-0 object-cover w-full h-full">
+    <div class="absolute top-0 left-0 z-20 flex items-center justify-center w-1/2 h-full">
+        <form wire:submit="register">
+            <div class="flex items-center justify-center">
+                <span class="text-5xl font-bold mb-14">Register</span>
+            </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <!-- Username -->
+            <div class="mt-4">
+                <x-input-label class="text-[#7c838a]" for="username" value="Username" />
+                <x-text-input wire:model="username" id="username"
+                            class="block w-full mt-1 bg-transparent border-[#656ed3] text-[#afb3ff]"
+                            type="text"
+                            name="username"
+                            required
+                            style="border-radius: 999px"
+                            autocomplete="username" />
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label class="text-[#7c838a]" for="password" :value="__('Password')" />
 
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                <x-text-input wire:model="password" id="password" class="block w-full mt-1 bg-transparent border-[#656ed3] text-[#afb3ff]"
+                                type="password"
+                                name="password"
+                                style="border-radius: 999px"
+                                required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label class="text-[#7c838a]" for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block w-full mt-1 bg-transparent border-[#656ed3] text-[#afb3ff]"
+                                type="password"
+                                style="border-radius: 999px"
+                                name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button class="block w-full py-3 mt-3 rounded-full" style="border-radius: 999px">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
 </div>
